0576353c6c727c8eb894ee6b9c7e1c9342684c5d
DuskXi <DuskXi>
2021-11-22 22:23:04 +0000

    update

--- client/test.py
+++ client/test.py
@@ -11,6 +11,8 @@ from pydub import AudioSegment
 import sys
 import requests
 import asyncio
+import JModel
+import datamodel
 
 from paralleldownload import ParallelDownload
 
@@ -52,14 +54,19 @@ def test():
     # play(r"D:\projects\playTogather\client\cache\JhewXi1UeqQ.m4a")
     # playLayer = PlayLayer(
     #     "https://r2---sn-5n3-n1ql.googlevideo.com/videoplayback?expire=1637103510&ei=NuOTYZHiJfqoxN8P-5-QwAQ&ip=31.205.236.29&id=o-AIrJzfVFpLchikfEvkPSYHwwOpvDmx5oQKvLbUjvxMyb&itag=140&source=youtube&requiressl=yes&mh=kw&mm=31%2C29&mn=sn-5n3-n1ql%2Csn-aigzrnld&ms=au%2Crdu&mv=m&mvi=2&pcm2cms=yes&pl=19&initcwndbps=2156250&vprv=1&mime=audio%2Fmp4&gir=yes&clen=3896046&dur=240.674&lmt=1613798414511546&mt=1637081588&fvip=3&keepalive=yes&fexp=24001373%2C24007246&c=ANDROID&txp=6311222&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgXwwCxGSugwdxfrC661NYCz_8X3_EqO0sV4jlzVozDkcCIQDjTG3LhlWbkD7zofv4mDOrGwqVCwn5k1E1Ez5JRugDgA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgW2218DT_KDsHs8W8M7HjJ5Mr3D8LF3yLnCofjnQOelYCICxeF0UIgqh5ik7MbrEyIpWfHl50_9v-G7tB3O6c_u5j")
-    playLayer = PlayLayer(r"D:\projects\playTogather\client\cache/JhewXi1UeqQ.m4a")
-    playLayer.benchmark_time = time.time()
-    playLayer.isRun = True
-    playLayer.play()
-    playLayer.set_position(20)
-    time.sleep(10)
-    playLayer.stop()
-    time.sleep(1000)
+    # playLayer = PlayLayer(r"D:\projects\playTogather\client\cache/JhewXi1UeqQ.m4a")
+    # playLayer.benchmark_time = time.time()
+    # playLayer.isRun = True
+    # playLayer.play()
+    # playLayer.set_position(20)
+    # time.sleep(10)
+    # playLayer.stop()
+    # time.sleep(1000)
+    # result = JModel.to_object({"testNumber": 0, "testClass": {"name": 123}}, JModel.TestObject)
+    result = datamodel.DataModel(json='{"playStatus":"play"}')
+    print(result)
+    changed = result.update('{"playStatus":"pause", "benchmarkDevice":"win", "benchmarkTimestamp": 123}')
+    print(result)
 
 
 if __name__ == '__main__':
